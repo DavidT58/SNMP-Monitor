1090e9c795b6ea70aa6cb12ca8f3bca852d9479c
DavidT58 <cd180700d@student.etf.bg.ac.rs>
2020-11-22 15:34:28 +0100

    test

--- src/snmpmon/BGPTable.java
+++ src/snmpmon/BGPTable.java
@@ -2,6 +2,7 @@ package snmpmon;
 
 import java.io.IOException;
 
+import com.ireasoning.protocol.Session;
 import com.ireasoning.protocol.snmp.*;
 
 public class BGPTable {
@@ -11,6 +12,8 @@ public class BGPTable {
 	private int port;
 	private String ip, rdc, wrc;
 	
+	
+	
 	public BGPTable(String ipa) throws IOException{
 		ip = ipa;
 		rdc = wrc = "si2019";
@@ -19,13 +22,19 @@ public class BGPTable {
 		session = new SnmpSession(target);
 	}
 	
-	public SnmpVarBind[] getLocalPreferenceAttribute() throws IOException{
+	public SnmpVarBind[] getLPAttr() throws IOException{
 		return session.snmpGetTableColumn(".1.3.6.1.2.1.15.6.1.12");
 	}
 	
 	public static void main(String[] args) throws IOException {
 		BGPTable table = new BGPTable("192.168.30.1");
-		System.out.println(table.getLocalPreferenceAttribute()[0]);
+		MibUtil.loadMib2();
+		String s = MibUtil.translateOID(".1.3.6.1.2.1.", false);
+		System.out.println(s);
+		/*
+		for(int i = 0; i < table.getLPAttr().length; i++)
+			System.out.println(table.getLPAttr()[i].getValue());
+		*/
 	}
 	
 }
